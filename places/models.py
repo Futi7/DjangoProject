d525569329eea1fd8d0455deb806feebbecd2047
futi7 <fuatseven1997@hotmail.com>
2020-03-28 17:56:11 +0300

    End of the 2nd video

---
+++ places/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Category(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır')
+        )
+    
+    
+    
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    keywords = models.CharField(max_length=200)
+    status = models.CharField(max_length=10, choices=STATUS)
+    image = models.ImageField(blank='True', upload_to='images/')
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

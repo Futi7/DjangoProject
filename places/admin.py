2b3537786bb75d73b1e0ee1dd4a9bf828c6a98f9
futi7 <fuatseven1997@hotmail.com>
2020-03-31 22:19:48 +0300

    End of the 4th video

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from places.models import Category
+from places.models import Category, Places
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+class PlacesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'status']
+    list_filter = ['status', 'category', 'price']
+
 
 
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Places, PlacesAdmin)
--- places/models.py
+++ places/models.py
@@ -6,9 +6,7 @@ class Category(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır')
         )
-    
-    
-    
+
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200, blank='True')
     keywords = models.CharField(max_length=200, blank='True')
@@ -16,6 +14,32 @@ class Category(models.Model):
     image = models.ImageField(blank='True', upload_to='images/')
     slug = models.SlugField()
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Places(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır')
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank='True')
+    details = models.TextField()
+    keywords = models.CharField(max_length=200, blank='True')
+    status = models.CharField(max_length=10, choices=STATUS)
+    image = models.ImageField(blank='True', upload_to='images/')
+    price = models.FloatField()
+    country = models.CharField(max_length=20, blank='True')
+    audience = models.CharField(max_length=20, blank='True')
 
 
     createdAt = models.DateTimeField(auto_now_add=True)

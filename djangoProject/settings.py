29610e33e40a92c6b28df75ab3f1c65fd210b541
Fuat Seven <fuatseven1997@hotmail.com>
2020-06-22 21:07:06 +0300

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
